9e8b4719497b3ec7b7275bfcfaa65b8ab78162d5
Michael <michael.scully1997@gmail.com>
2021-12-13 02:46:00 -0500

    Initial commit

---
+++ test/scrap_map.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ScrapMap from '../lib/scrap_map-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/scrap_map-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ScrapMap.ScrapMapStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
